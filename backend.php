461d849918550a65aac59a8f32d6a87be33da8eb
root <root@anonymous.anonymous>
2022-12-09 04:07:48 +0700

    first commit

---
+++ backend.php
@@ -0,0 +1,8 @@
+   include dari koneksi dan memilih data dari tabel produk
+    <?php
+  			include 'config.php';
+  			$stmt = $conn->prepare('SELECT * FROM product');
+  			$stmt->execute();
+  			$result = $stmt->get_result();
+  			while ($row = $result->fetch_assoc()):
+  		?>
